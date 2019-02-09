0f8eb4771e419be6aa70eaa1c5b51f20794c7475
Rahat <rahatmedeubai@gmail.com>
2019-02-09 17:49:25 +0600

    lab1

---
+++ Lab1/Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Student
+    {
+        public string name;
+        public string id;
+        public int year_of_study;
+        public void PrintInfo()
+        {
+            Console.Write(name + " " + id + " " + (year_of_study+1));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student A = new Student();
+            A.name = Convert.ToString(Console.ReadLine());
+            A.id = Convert.ToString(Console.ReadLine());
+            A.year_of_study = Convert.ToInt32(Console.ReadLine());
+            A.PrintInfo();
+
+        }
+    }
+}
---
+++ Lab1/task1_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int cnt = 0;
+            bool flag = false;
+           
+           for(int i = 0;i < n;i++)
+            {
+                flag = false;
+                if (a[i] <= 1) continue;
+                else
+                    for (int j = 2; j < a[i]; j++)
+                        if (a[i] % j == 0) flag = true;
+                if (!flag) cnt++;
+            }
+            Console.WriteLine(cnt);
+            for (int i = 0; i < n; i++)
+            {
+                flag = false;
+                if (a[i] <= 1) continue;
+                else
+                    for (int j = 2; j < a[i]; j++)
+                        if (a[i] % j == 0) flag = true;
+                if (!flag) Console.Write(a[i] + " ");    
+            }
+            Console.ReadKey();
+        }
+    }
+   }
+
