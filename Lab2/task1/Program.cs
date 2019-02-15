7fc09d5fd45b11ff616ea7568af998711a672d5b
MedeubayRahat <47471918+MedeubayRahat@users.noreply.github.com>
2019-02-15 23:18:57 +0600

    Commentik

---
+++ Lab2/task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = File.ReadAllText(@"C:\Users\PChelper\Desktop\qwerty\asdfg\input.txt");
+            //В определенном файле находим txt файл и его присваеваем в path
+            string newpath = new string(path.ToCharArray().Reverse().ToArray());
+            //стринг path делаем реверс и его присваеваем в newpath
+            string text = "Yes";
+            string text2 = "No";
+            if (path == newpath)  //сравниваем оба массива
+            {
+                File.WriteAllText(@"C:\Users\PChelper\Desktop\qwerty\output.txt", text);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Yes");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                File.WriteAllText(@"C:\Users\PChelper\Desktop\qwerty\output.txt", text2);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("No");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+    }
+}
+
---
+++ Lab2/task4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aaa = "Hello!";
+
+            string path2 = @"C:\Users\PChelper\Desktop\qwerty\output.txt";
+            StreamWriter file = new StreamWriter(@"C:\Users\PChelper\Desktop\qwerty\qwert\Test.txt");
+            file.Write(aaa);
+            file.Close();
+            string path = @"C:\Users\PChelper\Desktop\qwerty\qwert\Test.txt";
+            if (File.Exists(path2))
+            //Проверяем есть ли существующий файл , типо , Метод Move нельзя использовать для перезаписи существующего файла.
+            //Exists(file): определяет, существует ли файл
+            {
+                File.Delete(path2);     //Если есть ,то удаляем.
+                File.Move(path, path2);    //После файл с path перемещяем в path2
+                                           //Move: перемещает файл в новое место
+
+            }
+        }
+    }
+}
